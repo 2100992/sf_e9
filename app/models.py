ceb4c079c9b10747c407fae0e7b2544f65d58ee2
Shvd <2100992@gmail.com>
2020-04-10 16:15:33 +0300

    new models for homework

--- app/models.py
+++ app/models.py
@@ -4,18 +4,13 @@ from werkzeug.security import generate_password_hash,  check_password_hash
 from flask_login import UserMixin
 
 
-class Forecast(db.Model):
-    _id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, unique=False, nullable=False)
-    city = db.Column(db.String(80), unique=False, nullable=False)
-    temperature = db.Column(db.String(10), unique=False, nullable=False)
-    precipitation = db.Column(db.Boolean, unique=False, nullable=True)
-
-
 class Event(db.Model):
     __tablename__ = 'events'
+
     _id = db.Column(db.Integer, primary_key=True)
-    author = db.relationship('User', backref='event', )
+
+    author = db.Column(db.Integer, db.ForeignKey('users._id'))
+
     start_time = db.Column(db.DateTime())
     end_time = db.Column(db.DateTime())
     title = db.Column(db.String(100), nullable=False)
@@ -29,6 +24,7 @@ class Event(db.Model):
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
+
     _id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(100), nullable=False)
@@ -37,11 +33,13 @@ class User(db.Model, UserMixin):
     updated_on = db.Column(
         db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
 
+    # event_id = db.relationship('Event', backref='user', lazy=True)
+
     def __init__(self, email: str):
         self.email = email
 
     def get_id(self):
-        return self.email
+        return self._id
 
     def is_authenticated(self):
         return self.authenticated
