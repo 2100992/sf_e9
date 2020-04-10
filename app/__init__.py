3affe2defeb0eacec16de9035517782eba478ae3
Shvd <2100992@gmail.com>
2020-04-10 16:13:42 +0300

    to hell a Bcrypt

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
 
 from config import Config
 
@@ -13,7 +12,5 @@ db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-bcrypt = Bcrypt(app)
 
 from app import routes, models, forms
