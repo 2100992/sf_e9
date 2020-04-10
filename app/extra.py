01db3feddbd1f16c867de48bfe679381866f6268
Shvd <2100992@gmail.com>
2020-04-10 16:14:12 +0300

    cleaning for homework

--- app/extra.py
+++ app/extra.py
@@ -2,18 +2,18 @@ from datetime import timedelta, datetime
 from random import randint
 
 
-class Week:
-    def __init__(self, start=datetime.now()) -> None:
-        self.start = start.strftime('%d-%m-%y')
-        self.end = (start + timedelta(days=7)).strftime('%d-%m-%y')
-        self.week_days = self.get_weekdays(start)
+# class Week:
+#     def __init__(self, start=datetime.now()) -> None:
+#         self.start = start.strftime('%d-%m-%y')
+#         self.end = (start + timedelta(days=7)).strftime('%d-%m-%y')
+#         self.week_days = self.get_weekdays(start)
 
-    def get_weekdays(self, start):
-        weekdays = []
-        for i in range(8):
-            weekdays.append((start + timedelta(days=i)).strftime('%d-%m-%y'))
-        return weekdays
+#     def get_weekdays(self, start):
+#         weekdays = []
+#         for i in range(8):
+#             weekdays.append((start + timedelta(days=i)).strftime('%d-%m-%y'))
+#         return weekdays
 
 
-def get_weather_for_date(day):
-    return randint(5, 15)
+# def get_weather_for_date(day):
+#     return randint(5, 15)
